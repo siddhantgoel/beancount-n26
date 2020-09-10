94c7d4eace55a469d6e08fc12b8760222a009269
ArthurFDLR <arthfind@gmail.com>
2020-09-10 18:55:18 -0500

    Add test for file_date

--- tests/test_n26_importer.py
+++ tests/test_n26_importer.py
@@ -83,6 +83,9 @@ def test_extract_single_transaction(importer, filename):
 
     with open(filename) as fd:
         transactions = importer.extract(fd)
+        date = importer.file_date(fd)
+
+    assert date == datetime.date(2019, 10, 10)
 
     assert len(transactions) == 1
     assert transactions[0].date == datetime.date(2019, 10, 10)
@@ -101,8 +104,9 @@ def test_extract_multiple_transactions(importer, filename):
             _format(
                 '''
                 {header}
-                "2019-10-10","MAX MUSTERMANN","{iban_number}","Income","Muster GmbH","Income","-56.78","","",""
-                "2019-10-10","Muster GmbH","{iban_number}","Outgoing Transfer","Muster payment","Income","-12.34","","",""
+                "2019-12-28","MAX MUSTERMANN","{iban_number}","Income","Muster GmbH","Income","-56.78","","",""
+                "2020-01-05","Muster SARL","{iban_number}","Outgoing Transfer","Muster Fr payment","Income","-42.24","","",""
+                "2020-01-03","Muster GmbH","{iban_number}","Outgoing Transfer","Muster De payment","Income","-12.34","","",""
                 ''',  # NOQA
                 language=importer.language,
             )
@@ -110,12 +114,14 @@ def test_extract_multiple_transactions(importer, filename):
 
     with open(filename) as fd:
         transactions = importer.extract(fd)
+        date = importer.file_date(fd)
 
-    assert len(transactions) == 2
+    assert date == datetime.date(2020, 1, 5)
+    assert len(transactions) == 3
 
     # first
 
-    assert transactions[0].date == datetime.date(2019, 10, 10)
+    assert transactions[0].date == datetime.date(2019, 12, 28)
     assert transactions[0].payee == 'MAX MUSTERMANN'
     assert transactions[0].narration == 'Muster GmbH'
 
@@ -126,11 +132,22 @@ def test_extract_multiple_transactions(importer, filename):
 
     # second
 
-    assert transactions[1].date == datetime.date(2019, 10, 10)
-    assert transactions[1].payee == 'Muster GmbH'
-    assert transactions[1].narration == 'Muster payment'
+    assert transactions[1].date == datetime.date(2020, 1, 5)
+    assert transactions[1].payee == 'Muster SARL'
+    assert transactions[1].narration == 'Muster Fr payment'
 
     assert len(transactions[1].postings) == 1
     assert transactions[1].postings[0].account == 'Assets:N26'
     assert transactions[1].postings[0].units.currency == 'EUR'
-    assert transactions[1].postings[0].units.number == Decimal('-12.34')
+    assert transactions[1].postings[0].units.number == Decimal('-42.24')
+
+    # third
+
+    assert transactions[2].date == datetime.date(2020, 1, 3)
+    assert transactions[2].payee == 'Muster GmbH'
+    assert transactions[2].narration == 'Muster De payment'
+
+    assert len(transactions[2].postings) == 1
+    assert transactions[2].postings[0].account == 'Assets:N26'
+    assert transactions[2].postings[0].units.currency == 'EUR'
+    assert transactions[2].postings[0].units.number == Decimal('-12.34')
